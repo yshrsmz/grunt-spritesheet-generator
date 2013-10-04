842acfdf16d4d8030b3846f541c5d1e84d37cbee
yshrsmz <the.phantom.bane@gmail.com>
2013-10-04 12:05:25 +0900

    add jshint

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,4 +1,20 @@
 module.exports = function(grunt) {
 
-    grunt.initConfig({});
+    grunt.initConfig({
+        jshint: {
+            options: {
+                node: true,
+                nomen: false
+            },
+            task: {
+                src: [
+                    'tasks/**/*.js'
+                ]
+            }
+        }
+    });
+
+    grunt.loadNpmTasks('grunt-contrib-jshint');
+
+    grunt.registerTask('default', ['jshint']);
 };
